be4fe7726df7319251a062f28d76191301a2a8a2
taindp98 <taindp98@gmail.com>
2021-03-09 11:07:14 +0700

    mar9

--- app.py
+++ app.py
@@ -1,29 +1,8 @@
-#import requests
-#from flask import Flask, render_template, request
+
 import json
 #import telebot
 import requests
 
-#api_key='1607480015:AAFRjjzwhq5FLcwTFgde1gBzjc5v-g5Imck'
-# def get_bot_response():
-#     url = "https://api.telegram.org/bot{}/getUpdates".format(api_key)
-#     response = requests.get(url)
-#     data=response.json()
-#     # userText = request.args.get('msg')
-#     userText = data['result'][-1]['message']['text']
-#     # print(data['result'][-1]['message'])
-#     userID = data['result'][-1]['message']['chat']['id']
-#     api_url = 'http://0.0.0.0:6969/api/convers-manager'
-#     input_data = {}
-#     input_data['message'] = str(userText)
-#     input_data['state_tracker_id'] = str(userID)
-#     r = requests.post(url=api_url, json=input_data)
-#     chatbot_respose = r.json()
-#     mess_response = chatbot_respose['message'].replace('\n', r'').replace(r'"',r'')
-#     url_response='https://api.telegram.org/bot'+str(api_key)+'/sendmessage?chat_id='+str(userID)+'&text='+str(mess_response)
-#     response = requests.get(url_response)
-#     return response
-#
 import re
 from flask import Flask, request
 import telegram
@@ -54,27 +33,16 @@ def respond():
    # for debugging purposes only
    print("got text message :", text)
    # the first time you chat with the bot AKA the welcoming message
-   if text == "/start":
-       # print the welcoming message
-       bot_welcome = """
-       Welcome to coolAvatar bot, the bot is using the service from http://avatars.adorable.io/ to generate cool looking avatars based on the name you enter so please enter a name and the bot will reply with an avatar for your name.
-       """
-       # send the welcoming message
-       bot.sendMessage(chat_id=chat_id, text=bot_welcome, reply_to_message_id=msg_id)
-
-
-   else:
-       try:
-           # clear the message we got from any non alphabets
-           text = re.sub(r"W", "_", text)
-           # create the api link for the avatar based on http://avatars.adorable.io/
-           url = "https://api.adorable.io/avatars/285/{}.png".format(text.strip())
-           # reply with a photo to the name the user sent,
-           # note that you can send photos by url and telegram will fetch it for you
-           bot.sendPhoto(chat_id=chat_id, photo=url, reply_to_message_id=msg_id)
-       except Exception:
-           # if things went wrong
-           bot.sendMessage(chat_id=chat_id, text="There was a problem in the name you used, please enter different name", reply_to_message_id=msg_id)
+   
+   chatbot_sys_api_url = 'https://chatbot-hcmut.herokuapp.com/api/convers-manager'
+   input_data = {}
+   input_data['message'] = str(text)
+   input_data['state_tracker_id'] = chat_id
+   r = requests.post(url=chatbot_sys_api_url, json=input_data)
+   chatbot_respose = r.json()
+   mess_response = chatbot_respose['message'].replace('\n', r'').replace(r'"',r'')
+   
+   bot.sendMessage(chat_id=chat_id, text=mess_response, reply_to_message_id=msg_id)
 
    return 'ok'
 
